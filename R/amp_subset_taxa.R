e271cd854b094771acc7cdc180ae3ad7e49b168c
Rasmus Kirkegaard <rasmus.kirkegaard@hotmail.com>
2017-06-22 10:14:05 +0200

    Updated ampsubsettaxa
    Updated amp subset taxa to work for genus

--- R/amp_subset_taxa.R
+++ R/amp_subset_taxa.R
@@ -2,12 +2,12 @@
 #'
 #' Subset data based on taxonomy.
 #'
-#' @usage amp_subset_taxa(data, ...)
+#' @usage amp_subset_taxa(data, tax_vector)
 #'
-#' @param data (required) A object.
+#' @param data (required) A list object with 4 dataframes: abund, tax, metadata and refseq.
 #' @param tax_vector (required) a vector with taxonomic groups e.g. c("p__Chloroflexi","p__Actinobacteria")
 #' 
-#' @return A list object.
+#' @return A list object with 4 dataframes: abund, tax, metadata and refseq.
 #' 
 #' @export
 #' 
@@ -20,25 +20,25 @@ amp_subset_taxa<- function(data, tax_vector=c("p__Chloroflexi","p__Actinobacteri
      !any(names(data) == "abund") |
      !any(names(data) == "tax") | 
      !any(names(data) == "metadata") | 
+     !any(names(data) == "refseq") | 
      !is.data.frame(data[["abund"]]) |
      !is.data.frame(data[["tax"]]) |
+     !is.data.frame(data[["refseq"]]) |
      !is.data.frame(data[["metadata"]])
   ) {
-    stop("The data must be a list with three dataframes named abund, tax and metadata")
+    stop("The data must be a list with four dataframes named abund, tax, metadata, and refseq")
   }
   
   # Make selection
-  #selection<-which(data$tax$...)
   selection<-c(which(data$tax$Kingdom %in% tax_vector),
                which(data$tax$Phylum %in% tax_vector),
                which(data$tax$Class %in% tax_vector),
                which(data$tax$Order %in% tax_vector),
                which(data$tax$Family %in% tax_vector),
+               which(data$tax$Genus %in% tax_vector),
                which(data$tax$Species %in% tax_vector),
                which(data$tax$OTU %in% tax_vector)
-               )
-  
-  #newDF <- subset(oldDF, ...)
+  )
   
   # Make new list
   d1<-data$abund[selection,]
@@ -46,6 +46,6 @@ amp_subset_taxa<- function(data, tax_vector=c("p__Chloroflexi","p__Actinobacteri
   d3<-data$metadata
   d4<-data$refseq[selection]
   data<- list(abund = d1, tax = d2, metadata = d3, refseq = d4)
-
+  
   return(data)
 }
