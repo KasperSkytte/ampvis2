e316e0f668ba078886c75d02a297329e8fabbcf4
KasperSkytte <knaldhat@gmail.com>
2017-08-16 16:53:43 +0200

    Working on a documentation webpage

--- R/amp_ordinate.R
+++ R/amp_ordinate.R
@@ -4,70 +4,93 @@
 #'
 #' @usage amp_ordinate(data, type = "", transform = "", distmeasure = "", constrain = "")
 #'
-#' @param data (required) Data list as loaded with \code{amp_load()}.
-#' @param filter_species Remove low abundant OTU's across all samples below this threshold in percent. Setting this to 0 drastically increases the computation time. (Default: `0.1`)
-#' @param type (required) Type of ordination method. One of:
-#'   * `"PCA"`: (default) Principal Components Analysis
-#'   * `"RDA": Redundancy Analysis
-#'   * `"NMDS": non-metric Multidimensional Scaling
-#'   * `"PCOA" or `"MMDS"`: metric Multidimensional Scaling a.k.a Principal Coordinates Analysis (not to be confused with PCA)
-#'   * `"CA"`: Correspondence Analysis
-#'   * `"CCA"`: Canonical Correspondence Analysis
-#'   * `"DCA"`: Detrended Correspondence Analysis
-#' @param distmeasure (required for nMDS and PCoA) Distance measure used for the distance-based ordination methods (nMDS and PCoA), choose one of the following: `"manhattan"`, `"euclidean"`, `"canberra"`, `"bray"`, `"kulczynski"`, `"jaccard"`, `"gower"`, `"jsd"` (Jensen-Shannon Divergence), `"altGower"`, `"morisita"`, `"horn"`, `"mountford"`, `"raup"`, `"binomial"`, `"chao"`, `"cao"`, `"mahalanobis"` or simply `"none"` or `"sqrt"`. See details in `?vegdist`. JSD is stolen from \url{http://enterotype.embl.de/enterotypes.html}.
-#' @param transform (recommended) Transforms the abundances before ordination, choose one of the following: `"total"`, `"max"`, `"freq"`, `"normalize"`, `"range"`, `"standardize"`, `"pa"` (presence/absense), `"chi.square"`, `"hellinger"`, `"log"`, or `"sqrt"`, see details in ?decostand. Using the hellinger transformation is always a good choice and is recommended for PCA/RDA/nMDS/PCoA to obtain a more ecologically meaningful result (read about the double-zero problem). 
-#' @param constrain (required for RDA and CCA) Variable(s) in the metadata for constrained analyses (RDA and CCA). Multiple variables can be provided by a vector, fx c("Year", "Temperature"), but keep in mind that the more variables selected the more the result will be similar to unconstrained analysis.
-#' @param x_axis Which axis from the ordination results to plot as the first axis. Have a look at the `$screeplot` with `detailed_output = TRUE` to validate axes. (default: `1`)
-#' @param y_axis Which axis from the ordination results to plot as the second axis. Have a look at the `$screeplot` with `detailed_output = TRUE` to validate axes. (default: `2`)
+#' @param data (\emph{required}) Data list as loaded with \code{amp_load()}.
+#' @param filter_species Remove low abundant OTU's across all samples below this threshold in percent. Setting this to 0 drastically increases the computation time. (\emph{default}: \code{0.1})
+#' @param type (\emph{required}) Type of ordination method. One of:
+#' \itemize{
+#'    \item \code{"PCA"}: (\emph{default}) Principal Components Analysis
+#'    \item \code{"RDA"}: Redundancy Analysis
+#'    \item \code{"NMDS"}: non-metric Multidimensional Scaling
+#'    \item \code{"PCOA"} or \code{"MMDS"}: metric Multidimensional Scaling a.k.a Principal Coordinates Analysis (not to be confused with PCA)
+#'    \item \code{"CA"}: Correspondence Analysis
+#'    \item \code{"CCA"}: Canonical Correspondence Analysis
+#'    \item \code{"DCA"}: Detrended Correspondence Analysis
+#'    }
+#'    \emph{Note that PCoA is not performed by the vegan package, but the \code{\link[APE]{pcoa}} function from the APE package.}
+#' @param distmeasure (required for nMDS and PCoA) Distance measure used for the distance-based ordination methods (nMDS and PCoA), choose one of the following: \code{"manhattan"}, \code{"euclidean"}, \code{"canberra"}, \code{"bray"}, \code{"kulczynski"}, \code{"jaccard"}, \code{"gower"}, \code{"jsd"} (Jensen-Shannon Divergence), \code{"altGower"}, \code{"morisita"}, \code{"horn"}, \code{"mountford"}, \code{"raup"}, \code{"binomial"}, \code{"chao"}, \code{"cao"}, \code{"mahalanobis"} or simply \code{"none"} or \code{"sqrt"}. See details in \code{\link[vegan]{vegdist}}. JSD is based on \url{http://enterotype.embl.de/enterotypes.html}.
+#' @param transform (\emph{recommended}) Transforms the abundances before ordination, choose one of the following: \code{"total"}, \code{"max"}, \code{"freq"}, \code{"normalize"}, \code{"range"}, \code{"standardize"}, \code{"pa"} (presence/absense), \code{"chi.square"}, \code{"hellinger"}, \code{"log"}, or \code{"sqrt"}, see details in \code{\link[vegan]{decostand}}. Using the hellinger transformation is always a good choice and is recommended for PCA/RDA/nMDS/PCoA to obtain a more ecologically meaningful result (read about the double-zero problem in Numerical Ecology). 
+#' @param constrain (\emph{required for RDA and CCA}) Variable(s) in the metadata for constrained analyses (RDA and CCA). Multiple variables can be provided by a vector, fx \code{c("Year", "Temperature")}, but keep in mind that the more variables selected the more the result will be similar to unconstrained analysis.
+#' @param x_axis Which axis from the ordination results to plot as the first axis. Have a look at the \code{$screeplot} with \code{detailed_output = TRUE} to validate axes. (\emph{default:} \code{1})
+#' @param y_axis Which axis from the ordination results to plot as the second axis. Have a look at the \code{$screeplot} with \code{detailed_output = TRUE} to validate axes. (\emph{default:} \code{2})
 #' @param sample_color_by Color sample points by a variable in the metadata.   
-#' @param sample_color_order Order the colors in `sample_color_by` by the order in a vector. 
+#' @param sample_color_order Order the colors in \code{sample_color_by} by the order in a vector. 
 #' @param sample_label_by Label sample points by a variable in the metadata.
-#' @param sample_label_size Sample labels text size. (default: `4`)
-#' @param sample_label_segment_color Sample labels repel-segment color. (default: `"black"`)
+#' @param sample_label_size Sample labels text size. (\emph{default:} \code{4})
+#' @param sample_label_segment_color Sample labels repel-segment color. (\emph{default:} \code{"black"})
 #' @param sample_shape_by Shape sample points by a variable in the metadata.       
-#' @param sample_colorframe (logical) Frame the points with a polygon colored by the sample_color_by argument or not. (default: `FALSE`)
+#' @param sample_colorframe (\emph{logical}) Frame the points with a polygon colored by the sample_color_by argument or not. (\emph{default:} \code{FALSE})
 #' @param sample_colorframe_label Label by a variable in the metadata.
 #' @param sample_trajectory Make a trajectory between sample points by a variable in the metadata.
-#' @param sample_trajectory_group Make a trajectory between sample points by the `sample_trajectory` argument, but within individual groups.
-#' @param sample_plotly Enable interactive sample points so that they can be hovered to show additional information from the metadata. Provide a vector of the variables to show or `"all"` to display.
+#' @param sample_trajectory_group Make a trajectory between sample points by the \code{sample_trajectory} argument, but within individual groups.
+#' @param sample_plotly Enable interactive sample points so that they can be hovered to show additional information from the metadata. Provide a vector of the variables to show or \code{"all"} to display.
 #' 
-#' @param species_plot (logical) Plot species points or not. (default: `FALSE`)
-#' @param species_nlabels Number of the most extreme species labels to plot. Only makes sense with PCA/RDA
-#' @param species_label_taxonomy Taxonomic level by which to label the species points. (default: `"Genus"`)
-#' @param species_label_size Size of the species text labels. (default: `3`)
-#' @param species_label_color Color of the species text labels. (default: `"grey10`)
-#' @param species_rescale (logical) Rescale species points or not. Basically they will be multiplied by 0.8, for visual convenience only. (default: `FALSE`)
-#' @param species_size Size of the species points. (default: `2`)
-#' @param species_shape The shape of the species points, fx 1 for hollow circles or 20 for dots. (default: `20`)
-#' @param species_plotly (logical) Enable interactive species points so that they can be hovered to show complete taxonomic information about the OTU. (default: `FALSE`)
+#' @param species_plot (\emph{logical}) Plot species points or not. (\emph{default:} \code{FALSE})
+#' @param species_nlabels Number of the most extreme species labels to plot. Only makes sense with PCA/RDA.
+#' @param species_label_taxonomy Taxonomic level by which to label the species points. (\emph{default:} \code{"Genus"})
+#' @param species_label_size Size of the species text labels. (\emph{default:} \code{3})
+#' @param species_label_color Color of the species text labels. (\emph{default:} \code{"grey10"})
+#' @param species_rescale (\emph{logical}) Rescale species points or not. Basically they will be multiplied by 0.8, for visual convenience only. (\emph{default:} \code{FALSE})
+#' @param species_size Size of the species points. (\emph{default:} \code{2})
+#' @param species_shape The shape of the species points, fx \code{1} for hollow circles or \code{20} for dots. (\emph{default:} \code{20})
+#' @param species_plotly (\emph{logical}) Enable interactive species points so that they can be hovered to show complete taxonomic information about the OTU. (\emph{default:} \code{FALSE})
 #' 
-#' @param envfit_factor A vector of categorical environmental variables from the metadata to fit onto the ordination plot. See details in `?envfit`.
-#' @param envfit_numeric A vector of numerical environmental variables from the metadata to fit arrows onto the ordination plot. The lengths of the arrows are scaled by significance. See details in `?envfit`.
-#' @param envfit_signif_level The significance threshold for displaying the results of `envfit_factor` or `envfit_numeric`. (default: `0.001`)
-#' @param envfit_textsize Size of the envfit text on the plot. (default: `3`)
-#' @param envfit_color Color of the envfit text on the plot. (default: `"darkred"`)
-#' @param envfit_numeric_arrows_scale Scale the size of the numeric arrows. (default: `1`)
-#' @param envfit_show (logical) Show the results on the plot or not. (default: `TRUE`)
+#' @param envfit_factor A vector of categorical environmental variables from the metadata to fit onto the ordination plot. See details in \code{\link[vegan]{envfit}}.
+#' @param envfit_numeric A vector of numerical environmental variables from the metadata to fit arrows onto the ordination plot. The lengths of the arrows are scaled by significance. See details in \code{\link[vegan]{envfit}}.
+#' @param envfit_signif_level The significance threshold for displaying the results of \code{envfit_factor} or \code{envfit_numeric}. (\emph{default:} \code{0.001})
+#' @param envfit_textsize Size of the envfit text on the plot. (\emph{default:} \code{3})
+#' @param envfit_color Color of the envfit text on the plot. (\emph{default:} \code{"darkred"})
+#' @param envfit_numeric_arrows_scale Scale the size of the numeric arrows. (\emph{default:} \code{1})
+#' @param envfit_show (\emph{logical}) Show the results on the plot or not. (\emph{default:} \code{TRUE})
 #' 
-#' @param repel (logical) Repel all labels to prevent cluttering of the plot. (default: `TRUE`)
-#' @param opacity Opacity of all plotted points and sample_colorframe. 0: invisible, 1: opaque. (default: 0.8)
+#' @param repel (\emph{logical}) Repel all labels to prevent cluttering of the plot. (\emph{default:} \code{TRUE})
+#' @param opacity Opacity of all plotted points and sample_colorframe. \code{0}: invisible, \code{1}: opaque. (\emph{default:} \code{0.8})
 #' @param tax_empty How to show OTUs without taxonomic information. One of the following:
-#'   * `"remove"`: Remove OTUs without taxonomic information.
-#'   * `"best"`: (default) Use the best classification possible. 
-#'   * `"OTU"`: Display the OTU name.
-#' @param detailed_output (logical) Return additional details or not (model, scores, inputmatrix, screeplot etc). If `TRUE`, it is recommended to save to an object and then access the additional data by `View(object$data)`. (default: `FALSE`)
-#' @param ... Pass additional arguments to the vegan ordination functions, fx rda(...), cca(...), metaMDS(...), see vegan documentation.
+#' \itemize{
+#'    \item \code{"remove"}: Remove OTUs without taxonomic information.
+#'    \item \code{"best"}: (\emph{default}) Use the best classification possible. 
+#'    \item \code{"OTU"}: Display the OTU name.
+#'    }
+#' @param detailed_output (\emph{logical}) Return additional details or not (model, scores, inputmatrix, screeplot etc). If \code{TRUE}, it is recommended to save to an object and then access the additional data by \code{View(object$data)}. (\emph{default:} \code{FALSE})
+#' @param ... Pass additional arguments to the vegan ordination functions, fx the \code{\link[vegan]{rda}}, \code{\link[vegan]{cca}}, \code{\link[vegan]{metaMDS}} functions, see the documentation. 
 #'
-#' @return A ggplot2 object. If `detailed_output = TRUE` a list with a ggplot2 object and additional data.
+#' @return A ggplot2 object. If \code{detailed_output = TRUE} a list with a ggplot2 object and additional data.
 #'
 #' @details 
-#' The `amp_ordinate()` function is mainly based on two packages; vegan, which performs the actual ordination, and ggplot2 to generate the plot.
+#' The \code{amp_ordinate()} function is mainly based on two packages; \code{\link[vegan]{vegan-package}}, which performs the actual ordination, and \code{\link[ggplot2]{ggplot2-package}} to generate the plot.
 #' 
 #' @export
-#' 
+#' @references
+#'   GUide to STatistical Analysis in Microbial Ecology (GUSTA ME): \url{https://mb3is.megx.net/gustame}
+#'   
+#'   Legendre, Pierre & Legendre, Louis (2012). Numerical Ecology. Elsevier Science. ISBN: 9780444538680
+#'   
+#'   Legendre, P., & Gallagher, E. (2001). Ecologically meaningful transformations for ordination of species data. Oecologia, 129(2), 271-280. \url{http://doi.org/10.1007/s004420100716}
 #' @examples 
 #' data("AalborgWWTPs")
-#' amp_ordinate(AalborgWWTPs, type = "PCA", transform = "hellinger", sample_color_by = "Plant", sample_colorframe = TRUE)
+#' amp_ordinate(AalborgWWTPs,
+#'              type = "PCA",
+#'              transform = "hellinger",
+#'              sample_color_by = "Plant",
+#'              sample_colorframe = TRUE
+#'              )
+#' amp_ordinate(AalborgWWTPs,
+#'              type = "CCA",
+#'              transform = "Hellinger", 
+#'              constrain = "Period",
+#'              sample_color_by = "Period",
+#'              sample_colorframe = TRUE,
+#'              sample_colorframe_label = "Period"
+#'              ) + theme(legend.position = "blank")
 #' 
 #' @author Kasper Skytte Andersen \email{kasperskytteandersen@@gmail.com}
 #' @author Mads Albertsen \email{MadsAlbertsen85@@gmail.com}
