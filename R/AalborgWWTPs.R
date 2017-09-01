05334eb2a1e5d7914f358bf176beb84fb72360dd
KasperSkytte <knaldhat@gmail.com>
2017-09-01 12:37:45 +0200

    dataset documentation

--- R/AalborgWWTPs.R
+++ R/AalborgWWTPs.R
@@ -2,7 +2,8 @@
 #'
 #' A smaller subset of \code{\link{MiDAS}} with 50 samples from 2 Danish Wastewater Treatment Plants; Aalborg West and Aalborg East.
 #'
-#' @docType AalborgWWTPs
+#' @name AalborgWWTPs
+#' @docType data
 #'
 #' @usage data(AalborgWWTPs)
 #'
@@ -10,11 +11,6 @@
 #'
 #' @keywords datasets
 #' 
-#' @references Simon Jon McIlroy, Aaron Marc Saunders, Mads Albertsen, Marta Nierychlo, Bianca McIlroy, Aviaja Anna Hansen, Søren Michael Karst, Jeppe Lund Nielsen, Per Halkjær Nielsen; MiDAS: the field guide to the microbes of activated sludge, Database, Volume 2015, 1 January 2015, bav062
-#' (\href{https://doi.org/10.1093/database/bav062}{https://doi.org/10.1093/database/bav062})
-#'
-#' @source \href{http://midasfieldguide.org}{MiDAS: Field Guide to the Microbes of Activated Sludge and Anaerobic Digesters}
-#'
 #' @examples
 #' data(AalborgWWTPs)
 #' AalborgWWTPs
--- R/MiF.R
+++ R/MiF.R
@@ -2,6 +2,7 @@
 #'
 #' A data frame with known/unknown functional characteristics of the Genera in the MiDAS database. 
 #'
+#' @name MiF
 #' @docType MiF
 #'
 #' @usage data(MiF)
@@ -15,10 +16,5 @@
 #' }
 #'
 #' @keywords datasets
-#'
-#' @references Simon Jon McIlroy, Aaron Marc Saunders, Mads Albertsen, Marta Nierychlo, Bianca McIlroy, Aviaja Anna Hansen, Søren Michael Karst, Jeppe Lund Nielsen, Per Halkjær Nielsen; MiDAS: the field guide to the microbes of activated sludge, Database, Volume 2015, 1 January 2015, bav062
-#'  (\href{https://doi.org/10.1093/database/bav062}{https://doi.org/10.1093/database/bav062})
-#'
-#' @source \href{http://midasfieldguide.org}{MiDAS: Field Guide to the Microbes of Activated Sludge and Anaerobic Digesters}
 #'
 "MiDAS"
