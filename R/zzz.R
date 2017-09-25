8a4520cae69f21163cc97f91f734c3c527d17b2b
KasperSkytte <knaldhat@gmail.com>
2017-09-25 15:42:59 +0200

    Trying to load ggplot2 from onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,20 +1,22 @@
 .onAttach <- function(lib, pkg)  {
-  if (!interactive()) return()
-  local_version <- utils::packageVersion("ampvis2")
-  packageStartupMessage("This is ", pkg, " version ", local_version, appendLF = TRUE)
-  
-  if(requireNamespace('devtools', quietly = TRUE)) {
-    tryCatch({
-      github_ref <- remotes:::github_resolve_ref(
-        remotes::github_release(), 
-        remotes:::parse_git_repo("madsalbertsen/ampvis2@*release"))$ref
-      github_version <- package_version(gsub("v", "", github_ref))
-      if(local_version != github_version) {
-        packageStartupMessage(
-          'New version of ', pkg, ' (', github_version, ') is available! Install the latest release with \n\"devtools::install_github("madsalbertsen/ampvis2@*release").\"')
-      }
-    }, error=function(e) {
-      packageStartupMessage("Can't reach GitHub to check for new releases just now. Trying again next time. ")
-    })
+  require("ggplot2", quietly = TRUE, warn.conflicts = FALSE) #load ggplot2 without warnings
+  if (!interactive()) {
+    return()
+  } else {
+    packageStartupMessage("This is ", pkg, " version ", utils::packageVersion("ampvis2"), appendLF = TRUE)
+    if(requireNamespace('remotes', quietly = TRUE)) {
+      tryCatch({
+        github_ref <- remotes:::github_resolve_ref(
+          remotes::github_release(), 
+          remotes:::parse_git_repo("madsalbertsen/ampvis2@*release"))$ref
+        github_version <- package_version(gsub("v", "", github_ref))
+        if(local_version != github_version) {
+          packageStartupMessage(
+            'New version of ', pkg, ' (', github_version, ') is available! Install the latest release with \n\"devtools::install_github("madsalbertsen/ampvis2@*release").\"')
+        }
+      }, error=function(e) {
+        packageStartupMessage("Can't reach GitHub to check for new releases just now. Trying again next time. ")
+      })
+    }
   }
 }
