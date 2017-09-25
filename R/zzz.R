805042b3c41c04bec5971f0dde7f77600f768a15
KasperSkytte <knaldhat@gmail.com>
2017-09-25 15:32:59 +0200

    testing onAttach messages

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,20 @@
 .onAttach <- function(lib, pkg)  {
-  packageStartupMessage("This is ampvis version ",
-                        utils::packageDescription("ampvis2",
-                                                  fields="Version"),
-                        appendLF = TRUE)
+  if (!interactive()) return()
+  local_version <- utils::packageVersion("ampvis2")
+  packageStartupMessage("This is ", pkg, " version ", local_version, appendLF = TRUE)
+  
+  if(requireNamespace('devtools', quietly = TRUE)) {
+    tryCatch({
+      github_ref <- remotes:::github_resolve_ref(
+        remotes::github_release(), 
+        remotes:::parse_git_repo("madsalbertsen/ampvis2@*release"))$ref
+      github_version <- package_version(gsub("v", "", github_ref))
+      if(local_version != github_version) {
+        packageStartupMessage(
+          'New version of ', pkg, ' (', github_version, ') is available! Install the latest release with \n\"devtools::install_github("madsalbertsen/ampvis2@*release").\"')
+      }
+    }, error=function(e) {
+      packageStartupMessage("Can't reach GitHub to check for new releases just now. Trying again next time. ")
+    })
+  }
 }
