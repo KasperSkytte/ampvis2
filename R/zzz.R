ac8062e64dcaea981b12a46853a9453608f23bbe
KasperSkytte <knaldhat@gmail.com>
2017-09-25 16:00:13 +0200

    loading ggplot2 from onAttach to suppress warnings

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onAttach <- function(lib, pkg)  {
-  require("ggplot2", quietly = TRUE, warn.conflicts = FALSE) #load ggplot2 without warnings
+  suppressWarnings(require("ggplot2", quietly = TRUE, warn.conflicts = FALSE)) #load ggplot2 without warnings
   if (!interactive()) {
     return()
   } else {
     packageStartupMessage("This is ", pkg, " version ", utils::packageVersion("ampvis2"), appendLF = TRUE)
-    if(requireNamespace('remotes', quietly = TRUE)) {
+    if(requireNamespace("remotes", quietly = TRUE)) {
       tryCatch({
         github_ref <- remotes:::github_resolve_ref(
           remotes::github_release(), 
@@ -12,7 +12,7 @@
         github_version <- package_version(gsub("v", "", github_ref))
         if(local_version != github_version) {
           packageStartupMessage(
-            'New version of ', pkg, ' (', github_version, ') is available! Install the latest release with \n\"devtools::install_github("madsalbertsen/ampvis2@*release").\"')
+            "New release of ", pkg, " (", github_version, ") is available! Install the latest release of ", pkg, " with \ndevtools::install_github(\"madsalbertsen/ampvis2@*release\").")
         }
       }, error=function(e) {
         packageStartupMessage("Can't reach GitHub to check for new releases just now. Trying again next time. ")
