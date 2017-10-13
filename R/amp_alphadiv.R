732ee0cdcf9f564b84f2f8917a10dafdf1f3c503
KasperSkytte <knaldhat@gmail.com>
2017-10-13 16:29:08 +0200

    amp_alphadiv has been rewritten
    Simpler and faster

--- R/amp_alphadiv.R
+++ R/amp_alphadiv.R
@@ -1,6 +1,6 @@
 #' Alpha-diversity analysis
 #'
-#' Calculates alpha-diversity statistics for each sample and combines with the metadata.
+#' Calculate alpha-diversity indices for each sample and combines with the metadata.
 #'
 #' @usage amp_alphadiv(data, measure, rarefy)
 #'
@@ -12,20 +12,21 @@
 #'   \item \code{"simpson"}
 #'   \item \code{"invsimpson"}
 #' }
-#' @param rarefy Rarefy species richness to this value. Passed directly as the \code{sample} argument to \code{\link[vegan]{rrarefy}}. (\emph{default:} \code{NULL})
+#' @param richness (\emph{logical}) Also calculate sample richness estimates (Chao1 and ACE) as calculated by \code{\link[vegan]{estimateR}}. (\emph{default:} \code{FALSE})
+#' @param rarefy Rarefy species richness to this value before calculating alpha diversity and/or richness. Passed directly as the \code{sample} argument to \code{\link[vegan]{rrarefy}}. (\emph{default:} \code{NULL})
 #' 
 #' @export
 #' @import dplyr
 #' @import vegan
 #' 
-#' @details The alpha-diversity indices are calculated per sample using the vegan function \code{\link[vegan]{diversity}}, where species richness is first rarefied using \code{\link[vegan]{rrarefy}} by the size of the \code{rarefy} argument. Refer to the vegan documentation for details about the different indices and how they are calculated. If no measure(s) are chosen, all diversity indices will be returned.
+#' @details The alpha-diversity indices are calculated per sample using the vegan function \code{\link[vegan]{diversity}}, where the read abundances are first rarefied using \code{\link[vegan]{rrarefy}} by the size of the \code{rarefy} argument. Refer to the vegan documentation for details about the different indices and how they are calculated. If no measure(s) are chosen, all diversity indices will be returned.
 #' 
 #' @examples 
 #' #Load example data
 #' data("AalborgWWTPs")
 #' 
-#' #Calculate alpha-diversity indices and subsample/rarefy to 20000 reads
-#' alphadiversityresult <- amp_alphadiv(AalborgWWTPs, rarefy = 20000)
+#' #Subsample/rarefy to 20000 reads and then calculate Shannon and Simpson alpha-diversity indices
+#' alphadiversityresult <- amp_alphadiv(AalborgWWTPs, measure = c("shannon", "simpson"), rarefy = 20000)
 #' 
 #' #Explore the results in the data frame
 #' #View(alphadiversityresult)
@@ -34,27 +35,41 @@
 #' @author Kasper Skytte Andersen \email{kasperskytteandersen@gmail.com}
 #' @author Mads Albertsen \email{MadsAlbertsen85@@gmail.com}
 
-amp_alphadiv <- function (data, measure = NULL, rarefy = NULL) {
-  
+amp_alphadiv <- function (data,
+                          measure = NULL,
+                          richness = FALSE,
+                          rarefy = NULL) {
   ### Data must be in ampvis2 format
   if(class(data) != "ampvis2")
     stop("The provided data is not in ampvis2 format. Use amp_load() to load your data before using ampvis functions. (Or class(data) <- \"ampvis2\", if you know what you are doing.)")
   
-  abund <- data[["abund"]] %>% as.data.frame()
-  Reads <- colSums(abund)
-  ObservedOTUs = colSums(abund > 0)
-  metadata <- data[["metadata"]]
+  measure <- measure %>% tolower()
+  validMeasures <- c("observed", "shannon", "simpson", "invsimpson")
   
-  if(!is.null(rarefy)){
-    abund <- suppressWarnings(vegan::rrarefy(t(abund), sample = rarefy)) %>% t() %>% as.data.frame()
-    if(rarefy > max(colSums(data$abund)) | rarefy < min(colSums(data$abund))) {
-      warning("The chosen rarefy size is not within the min/max range of the data:\nMin#reads: ", as.character(min(colSums(data$abund))), "\nMax#reads: ", as.character(max(colSums(data$abund))))
-    }
-    if (min(colSums(abund)) < rarefy) {
-      warning("The following samples have not been rarefied (less than ", as.character(rarefy), " reads):\n", paste(rownames(metadata[which(colSums(abund) < rarefy),]), collapse = ", "))
-    }
+  #check measures
+  if(any(!measure %in% validMeasures)) {
+    warning("Some or none of the provided measures were not recognised, calculating all. Valid options are:\n", paste0(validMeasures, collapse = ", "))
+    measure <- validMeasures
   }
   
+  results <- data$metadata
+  results$Reads <- colSums(data$abund)
+  abund <- t(data$abund)
+  
+  if(!is.null(rarefy) & is.numeric(rarefy)){
+    if(rarefy > max(results$Reads) | rarefy < min(results$Reads)) {
+      stop("The chosen rarefy size is not within the min/max range of the data:\nMin#reads: ", as.character(min(colSums(data$abund))), "\nMax#reads: ", as.character(max(results$Reads)))
+    } else {
+      abund <- suppressWarnings(vegan::rrarefy(abund, sample = rarefy)) %>% as.data.frame()
+      if (min(results$Reads) < rarefy) {
+        message("The following samples have not been rarefied (less than ", as.character(rarefy), " reads):\n", paste(rownames(data$metadata[which(results$Reads < rarefy),]), collapse = ", "))
+      }
+    }
+  } else if(!is.null(rarefy) & !is.numeric(rarefy)) {
+    stop("Argument rarefy must be numerical.")
+  }
+  
+  #warning from phyloseq::estimate_richness
   if (!any(abund == 1)) {
     warning("The data you have provided does not have\n", 
             "any singletons. This is highly suspicious. Results of richness\n", 
@@ -63,53 +78,24 @@ amp_alphadiv <- function (data, measure = NULL, rarefy = NULL) {
             "We recommend that you find the un-trimmed data and retry.")
   }
   
-  abund <- as.matrix(t(abund))
-  
-  renamevec = c("observed", "chao1", "ace", "shannon", "simpson", 
-                "invsimpson")
-  
-  names(renamevec) <- c("s.obs", "s.chao1", "s.ace", "shannon", 
-                        "simpson", "invsimpson")
-  
-  if (is.null(measure)) {
-    measure = renamevec %>% as.character() %>% tolower()
+  if(any("observed" %in% measure) | is.null(measure)) {
+    results$ObservedOTUs <- colSums(t(abund) > 0)
   }
-  if (any(measure %in% names(renamevec))) {
-    measure[measure %in% names(renamevec)] <- renamevec[names(renamevec) %in% measure]
+  if(any("shannon" %in% measure)) {
+    results$Shannon = vegan::diversity(abund, index = "shannon")
   }
-  if (!any(measure %in% renamevec)) {
-    stop("None of the `measure`s provided are supported. Try default `NULL` instead.")
+  if(any("simpson" %in% measure)) {
+    results$Simpson <- vegan::diversity(abund, index = "simpson")
   }
-  
-  outlist = vector("list")
-  
-  estimRmeas = c("chao1", "observed", "ace")
-  
-  if (any(estimRmeas %in% measure)) {
-    outlist <- c(outlist, list(t(data.frame(vegan::estimateR(abund)))))
+  if(any("invsimpson" %in% measure)) {
+    results$invSimpson <- vegan::diversity(abund, index = "invsimpson")
   }
-  if ("shannon" %in% measure) {
-    outlist <- c(outlist, list(shannon = vegan::diversity(abund, index = "shannon")))
-  }
-  if ("simpson" %in% measure) {
-    outlist <- c(outlist, list(simpson = vegan::diversity(abund, index = "simpson")))
-  }
-  if ("invsimpson" %in% measure) {
-    outlist <- c(outlist, list(invsimpson = vegan::diversity(abund, index = "invsimpson")))
+  if(richness) {
+    richness <- t(vegan::estimateR(abund)) %>% as.data.frame()
+    results$Chao1 <- richness$S.chao1
+    results$ACE <- richness$S.ACE
   }
   
-  rich = do.call("cbind", outlist)
-  namechange = intersect(colnames(rich), names(renamevec))
-  
-  colnames(rich)[colnames(rich) %in% namechange] <- renamevec[namechange]
-  
-  colkeep = sapply(paste0("(se\\.){0,}", measure), grep, 
-                   colnames(rich), ignore.case = TRUE)
-  
-  rich = rich[, sort(unique(unlist(colkeep))), drop = FALSE]
-  
-  #Combine metadata and results and return
-  combined <- data.frame(metadata, Reads = Reads, ObservedOTUs = ObservedOTUs, rich) %>% arrange(Reads)
-  return(combined)
+  results <- results %>% arrange(Reads)
+  return(results)
 }
-
