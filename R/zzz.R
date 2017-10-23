30882d28bbc0a29ed14ac134d011dda4fb7d25d4
KasperSkytte <knaldhat@gmail.com>
2017-10-23 09:52:12 +0200

    same

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,8 @@
     #hide import conflict warnings
     ops <- options(warn = -1)
     on.exit(options(ops))
-    withCallingHandlers(attachNamespace("ggplot2"), warning = function(w) invokeRestart("muffleWarning"))
+    attachNamespace("ggplot2")
+    #withCallingHandlers(attachNamespace("ggplot2"), warning = function(w) invokeRestart("muffleWarning"))
     
     #print version on package load
     local_version <- utils::packageVersion("ampvis2")
