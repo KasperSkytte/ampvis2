a5fa16f88edcd8f94f6b3613e7618669003a2074
KasperSkytte <knaldhat@gmail.com>
2017-11-16 10:56:54 +0100

    Added argument so the taxa in tax_vector are the ones being removed
    and updated documentation

--- R/amp_subset_taxa.R
+++ R/amp_subset_taxa.R
@@ -1,19 +1,20 @@
 #' Subset ampvis2 objects based on taxonomy
 #'
-#' Subsets the data in ampvis2 objects based on taxonomy and returns the subsetted object. See examples.
+#' Subsets the data in ampvis2 objects based on taxonomy and returns the subsetted object. 
 #'
 #' @usage amp_subset_taxa(data, tax_vector)
 #'
 #' @param data (\emph{required}) Data list as loaded with \code{\link{amp_load}}.
-#' @param tax_vector (required) A vector with taxonomic groups, e.g. \code{c("p__Chloroflexi","p__Actinobacteria")}.
+#' @param tax_vector (required) A vector with the taxonomic groups with which to perform the subset. The prefixes \code{"k__"}, \code{"p__"}, \code{"c__"}, \code{"o__"}, \code{"f__"}, \code{"g__"}, \code{"s__"} indicates their taxonomic rank and the following characters their name (almost always with a capital first letter), e.g. \code{c("p__Chloroflexi","p__Actinobacteria")}.
 #' @param normalise (\emph{logical}) Normalise the read abundances to the total amount of reads (percentages) \emph{BEFORE} the subset. (\emph{default:} \code{FALSE})
+#' @param remove (\emph{logical}) If set to TRUE, then the taxa matching the provided vector will be removed instead of being the only ones kept in the data. (\emph{default:} \code{FALSE})
 #' 
 #' @return A list with 3 dataframes (4 if reference sequences are provided).
 #' @import ape
 #' @export
 #' 
 #' @details 
-#' The subset of taxa is done by providing a vector of taxa names, where all other taxa are then removed. The taxa names in the vector will be matched in all columns in the taxonomy table.
+#' The taxonomy subset is done by providing a \code{tax_vector} of taxa names which are then matched to the taxonomy table, where all other taxa not matching the \code{tax_vector} are removed. If \code{remove = TRUE}, then the matching taxa are the ones being removed instead. The taxa names in \code{tax_vector} will be matched in all columns of the taxonomy table.
 #' 
 #' @section Normalising data for use in heatmaps:
 #' By default the raw read counts in the abundance matrix are normalised (transformed to percentages) by \code{\link{amp_heatmap}} automatically. This means that the relative abundances shown will be calculated based on the remaining taxa after the subset, not including the removed taxa, if any. To circumvent this, set \code{normalise = TRUE} when subsetting with the \code{\link{amp_subset_taxa}} and \code{\link{amp_subset_samples}} functions and then set \code{raw = TRUE} when using \code{\link{amp_heatmap}}, see the example below.
@@ -34,16 +35,44 @@
 #' 
 #' @examples 
 #' #Load example data
-#' data("MiDAS")
+#' data("AalborgWWTPs")
 #' 
-#' #Show a short summary about the data by simply typing the name of the object in the console
-#' MiDAS
+#' #An overview heatmap of the data:
+#' amp_heatmap(AalborgWWTPs, 
+#'             tax_aggregate = "Genus",
+#'             group_by = "Plant",
+#'             tax_add = "Phylum"
+#'             )
 #' 
-#' #Remove all taxa except the phyla Chloroflexi and Actinobacteria
-#' MiDASsubset <- amp_subset_taxa(MiDAS, tax_vector = c("p__Chloroflexi", "p__Actinobacteria"))
+#' #Remove all taxa except the phyla Chloroflexi and Actinobacteria 
+#' #and the Genera Rhodoferax and Trichococcus:
+#' tax_vector <- c("p__Chloroflexi",
+#'             "p__Actinobacteria",
+#'             "g__Rhodoferax",
+#'             "g__Trichococcus")
+#'             
+#' AalborgWWTPs_subset <- amp_subset_taxa(AalborgWWTPs, 
+#'                                        tax_vector = tax_vector
+#'                                        )
 #' 
-#' #Summary
-#' MiDASsubset
+#' #The resulting subset:
+#' amp_heatmap(AalborgWWTPs_subset, 
+#'             tax_aggregate = "Genus",
+#'             group_by = "Plant",
+#'             tax_add = "Phylum"
+#'             )
+#' 
+#' #Or if remove = TRUE then the taxa in tax_vector are the ones being removed:
+#' AalborgWWTPs_subset <- amp_subset_taxa(AalborgWWTPs, 
+#'                                        tax_vector = tax_vector,
+#'                                        remove = TRUE
+#'                                        )
+#' #The resulting subset:
+#' amp_heatmap(AalborgWWTPs_subset, 
+#'             tax_aggregate = "Genus",
+#'             group_by = "Plant",
+#'             tax_add = "Phylum"
+#'             )
 #' 
 #' @seealso 
 #' \code{\link{amp_subset_samples}}, \code{\link{amp_heatmap}}
@@ -53,7 +82,10 @@
 #' @author Rasmus Hansen Kirkegaard \email{rhk@@bio.aau.dk}
 
 
-amp_subset_taxa <- function(data, tax_vector = c("p__Chloroflexi", "p__Actinobacteria"), normalise = FALSE) {
+amp_subset_taxa <- function(data, 
+                            tax_vector = c("p__Chloroflexi", "p__Actinobacteria"), 
+                            normalise = FALSE,
+                            remove = FALSE) {
   
   ### Data must be in ampvis2 format
   if(class(data) != "ampvis2")
@@ -66,10 +98,10 @@ amp_subset_taxa <- function(data, tax_vector = c("p__Chloroflexi", "p__Actinobac
   
   ### calculate percentages 
   if (normalise) {
-    data$abund <- apply(data$abund,2, function(x) 100*x/sum(x)) %>% as.data.frame() 
+    data$abund <- as.data.frame(apply(data$abund,2, function(x) 100*x/sum(x)))
   }
   
-  # Make selection
+  # Make new list
   selection <- c(which(data$tax$Kingdom %in% tax_vector),
                  which(data$tax$Phylum %in% tax_vector),
                  which(data$tax$Class %in% tax_vector),
@@ -78,16 +110,18 @@ amp_subset_taxa <- function(data, tax_vector = c("p__Chloroflexi", "p__Actinobac
                  which(data$tax$Genus %in% tax_vector),
                  which(data$tax$Species %in% tax_vector),
                  which(data$tax$OTU %in% tax_vector)
-                 )
-  
-  # Make new list
-  data$abund <- data$abund[selection,]
-  data$tax <- data$tax[selection,]
-  data$metadata <- data$metadata
+  )
+  selection <- unique(selection)
+  newtax <- data$tax[selection,]
+  if(isTRUE(remove)) {
+    data$tax <- subset(data$tax, !OTU %in% newtax$OTU)
+  } else if(!isTRUE(remove)) {
+    data$tax <- newtax
+  }
+  data$abund <- data$abund[rownames(data$tax),]
   
   if (any(names(data) == "refseq")) {
-    data$refseq <- data$refseq[selection]
+    data$refseq <- data$refseq[rownames(data$tax)]
   }
-
   return(data)
 }
