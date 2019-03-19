5d40a67d15a768fc94f5de377f081ac01bf018d6
Kasper Skytte Andersen <knaldhat@gmail.com>
2019-03-19 10:41:09 +0100

    print methods in function(x, ...) format

--- R/print_methods.R
+++ R/print_methods.R
@@ -1,14 +1,15 @@
 #' Prints heatmap+functions plot (internal function)
 #'
-#' @param hmfunplot plot
+#' @param x plot
+#' @param ... not used
 #'
 #' @export
 #' @importFrom cowplot plot_grid
-print.hmfunplot <- function(hmfunplot) {
-  print(cowplot::plot_grid(hmfunplot$heatmap,
-    hmfunplot$functions,
+print.hmfunplot <- function(x, ...) {
+  print(cowplot::plot_grid(x$heatmap,
+    x$functions,
     ncol = 2,
-    rel_widths = attributes(hmfunplot)[["rel_widths"]],
+    rel_widths = attributes(x)[["rel_widths"]],
     align = "h",
     axis = "tb"
   ))
@@ -16,62 +17,64 @@ print.hmfunplot <- function(hmfunplot) {
 
 #' Prints ampvis2 object summary (internal function)
 #'
-#' @param data (\emph{required}) Data list as loaded with \code{\link{amp_load}}.
+#' @param x (\emph{required}) Data list as loaded with \code{\link{amp_load}}.
+#' @param ... not used
 #'
 #' @importFrom crayon underline
 #' @export
 #' @author Kasper Skytte Andersen \email{ksa@@bio.aau.dk}
-print.ampvis2 <- function(data) {
+print.ampvis2 <- function(x, ...) {
   ### calculate basic statistics and useful information about the data, print it
-  if (!isTRUE(attributes(data)$normalised)) {
+  if (!isTRUE(attributes(x)$normalised)) {
     # calculate basic stats and store in attributes for use in print.ampvis2
-    readstats <- attributes(data)$readstats <- list(
-      "Total#Reads" = as.character(sum(data$abund)),
-      "Min#Reads" = as.character(min(colSums(data$abund))),
-      "Max#Reads" = as.character(max(colSums(data$abund))),
-      "Median#Reads" = as.character(median(colSums(data$abund))),
-      "Avg#Reads" = as.character(round(mean(colSums(data$abund)), digits = 2))
+    readstats <- attributes(x)$readstats <- list(
+      "Total#Reads" = as.character(sum(x$abund)),
+      "Min#Reads" = as.character(min(colSums(x$abund))),
+      "Max#Reads" = as.character(max(colSums(x$abund))),
+      "Median#Reads" = as.character(median(colSums(x$abund))),
+      "Avg#Reads" = as.character(round(mean(colSums(x$abund)), digits = 2))
     )
-  } else if (isTRUE(attributes(data)$normalised)) {
-    readstats <- attributes(data)$readstats
+  } else if (isTRUE(attributes(x)$normalised)) {
+    readstats <- attributes(x)$readstats
   }
-  cat(class(data), "object with", length(data), "elements.", crayon::underline("\nSummary of OTU table:\n"))
+  cat(class(x), "object with", length(x), "elements.", crayon::underline("\nSummary of OTU table:\n"))
   print.table(c(
-    "Samples" = as.character(ncol(data$abund)),
-    "OTUs" = as.character(nrow(data$abund)),
+    "Samples" = as.character(ncol(x$abund)),
+    "OTUs" = as.character(nrow(x$abund)),
     readstats
   ),
   justify = "right"
   )
-  if (isTRUE(attributes(data)$normalised)) {
+  if (isTRUE(attributes(x)$normalised)) {
     cat("(The read counts have been normalised)\n")
   }
   cat(crayon::underline("\nAssigned taxonomy:\n"))
   print.table(c(
-    "Kingdom" = paste0(sum(nchar(data$tax$Kingdom) > 3), "(", round(sum(nchar(data$tax$Kingdom) > 3) / nrow(data$abund), digits = 2) * 100, "%)"),
-    "Phylum" = paste0(sum(nchar(data$tax$Phylum) > 3), "(", round(sum(nchar(data$tax$Phylum) > 3) / nrow(data$abund) * 100, digits = 2), "%)"),
-    "Class" = paste0(sum(nchar(data$tax$Class) > 3), "(", round(sum(nchar(data$tax$Class) > 3) / nrow(data$abund) * 100, digits = 2), "%)"),
-    "Order" = paste0(sum(nchar(data$tax$Order) > 3), "(", round(sum(nchar(data$tax$Order) > 3) / nrow(data$abund) * 100, digits = 2), "%)"),
-    "Family" = paste0(sum(nchar(data$tax$Family) > 3), "(", round(sum(nchar(data$tax$Family) > 3) / nrow(data$abund) * 100, digits = 2), "%)"),
-    "Genus" = paste0(sum(nchar(data$tax$Genus) > 3), "(", round(sum(nchar(data$tax$Genus) > 3) / nrow(data$abund) * 100, digits = 2), "%)"),
-    "Species" = paste0(sum(nchar(data$tax$Species) > 3), "(", round(sum(nchar(data$tax$Species) > 3) / nrow(data$abund) * 100, digits = 2), "%)")
+    "Kingdom" = paste0(sum(nchar(x$tax$Kingdom) > 3), "(", round(sum(nchar(x$tax$Kingdom) > 3) / nrow(x$abund), digits = 2) * 100, "%)"),
+    "Phylum" = paste0(sum(nchar(x$tax$Phylum) > 3), "(", round(sum(nchar(x$tax$Phylum) > 3) / nrow(x$abund) * 100, digits = 2), "%)"),
+    "Class" = paste0(sum(nchar(x$tax$Class) > 3), "(", round(sum(nchar(x$tax$Class) > 3) / nrow(x$abund) * 100, digits = 2), "%)"),
+    "Order" = paste0(sum(nchar(x$tax$Order) > 3), "(", round(sum(nchar(x$tax$Order) > 3) / nrow(x$abund) * 100, digits = 2), "%)"),
+    "Family" = paste0(sum(nchar(x$tax$Family) > 3), "(", round(sum(nchar(x$tax$Family) > 3) / nrow(x$abund) * 100, digits = 2), "%)"),
+    "Genus" = paste0(sum(nchar(x$tax$Genus) > 3), "(", round(sum(nchar(x$tax$Genus) > 3) / nrow(x$abund) * 100, digits = 2), "%)"),
+    "Species" = paste0(sum(nchar(x$tax$Species) > 3), "(", round(sum(nchar(x$tax$Species) > 3) / nrow(x$abund) * 100, digits = 2), "%)")
   ),
   justify = "right"
   )
-  cat(crayon::underline("\nMetadata variables:"), as.character(ncol(data$metadata)), "\n", paste(as.character(colnames(data$metadata)), collapse = ", "))
+  cat(crayon::underline("\nMetadata variables:"), as.character(ncol(x$metadata)), "\n", paste(as.character(colnames(x$metadata)), collapse = ", "))
 }
 
 #' Print method for figure caption created by amp_ordinate
 #'
-#' @param captionwithrefs Character vector with the caption
+#' @param x Character vector with the caption
+#' @param ... not used
 #'
 #' @importFrom cli cat_line rule
 #' @importFrom crayon italic
 #' @export
 #' @author Kasper Skytte Andersen \email{ksa@@bio.aau.dk}
-print.figcaption <- function(captionwithrefs) {
+print.figcaption <- function(x, ...) {
   cli::cat_line(cli::rule("Auto-generated figure caption (start)"))
-  captionwithrefs %>%
+  x %>%
     strwrap() %>%
     crayon::italic() %>%
     cli::cat_line()
