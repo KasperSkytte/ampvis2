e6d07051744978add23e8cd03b44126720240868
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-06-24 14:47:52 +0200

    add tests and test data for loading BIOM and sintax format files

--- tests/testthat/test-amp_load.R
+++ tests/testthat/test-amp_load.R
@@ -195,3 +195,47 @@ test_that("loading data with unique OTU's in both otutable and taxonomy", {
     regexp = "The OTU's between otutable and taxonomy do not match exactly. 3 OTU's in taxonomy not present in otutable have been removed from taxonomy. 4 OTU's are missing "
   )
 })
+
+test_that("loading a (non-hdf5 format) BIOM file with no taxonomy fails", {
+  expect_error(
+    amp_load("../testdata/min_sparse_otu_table.biom")
+  )
+})
+
+test_that("loading a (hdf5 format) BIOM file with no taxonomy fails", {
+  expect_error(
+    amp_load("../testdata/min_sparse_otu_table_hdf5.biom")
+  )
+})
+
+test_that("loading a (non-hdf5 format) BIOM file returns an ampvis2 class object", {
+  expect_s3_class(
+    suppressWarnings(amp_load("../testdata/rich_sparse_otu_table.biom")),
+    class = "ampvis2",
+    exact = TRUE
+  )
+})
+
+test_that("loading a (hdf5 format) BIOM file returns an ampvis2 class object", {
+  expect_s3_class(
+    suppressWarnings(amp_load("../testdata/rich_sparse_otu_table_hdf5.biom")),
+    class = "ampvis2",
+    exact = TRUE
+  )
+})
+
+test_that("loading hdf5 and non-hdf5 format BIOM file returns identical ampvis2 class objects", {
+  biom_nonhdf5 <- suppressWarnings(amp_load("../testdata/rich_sparse_otu_table.biom"))
+  biom_hdf5 <- suppressWarnings(amp_load("../testdata/rich_sparse_otu_table_hdf5.biom"))
+  expect_identical(biom_hdf5, biom_nonhdf5)
+})
+
+test_that("loading sintax format taxonomy returns an ampvis2 class object", {
+  expect_s3_class(
+    suppressWarnings(
+      amp_load("tests/testdata/ASVtable.tsv", taxonomy = "tests/testdata/ASVs.sintax")
+    ),
+    class = "ampvis2",
+    exact = TRUE
+  )
+})
