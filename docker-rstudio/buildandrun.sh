900384929a17a377e95958a96b9cbbeacbabd3ba
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-07-06 14:12:34 +0200

    add some dependencies+use AAU CRAN mirror

--- docker-rstudio/buildandrun.sh
+++ docker-rstudio/buildandrun.sh
@@ -3,7 +3,7 @@ set -eu
 
 #set variables
 r_ver="4.1.0"
-ampvis2_rel="2.7.7"
+ampvis2_rel="2.7.8"
 image_name="kasperskytte/rstudio_r${r_ver}_ampvis2:${ampvis2_rel}"
 password="supersafepassword"
 port="8787" #will generate a random one if unavailable
@@ -12,7 +12,7 @@ RENV_PATHS_CACHE_CONTAINER="/usr/local/lib/R/renv-cache/" #path to renv cache wi
 num_threads=$(($(nproc) - 2)) #all cores except 2
 #done setting variables
 
-wget -q https://raw.githubusercontent.com/MadsAlbertsen/ampvis2/2.7.7/renv.lock -O renv.lock
+wget -q https://raw.githubusercontent.com/MadsAlbertsen/ampvis2/${ampvis2_rel}/renv.lock -O renv.lock
 
 cat << Dockerfile > Dockerfile
 FROM rocker/rstudio:${r_ver}
@@ -29,12 +29,20 @@ RUN apt-get update -qqy && \\
     libcairo2-dev \\
     libxt-dev \\
     libjpeg-dev \\
-    net-tools
+    net-tools \\
+    libharfbuzz-dev \\
+    libfribidi-dev \\
+    libfreetype6-dev \\
+    libpng-dev \\
+    libtiff5-dev \\
+    libjpeg-dev
 
-#set default renv cache path in container,
-#install renv, install all packages in the lock file to /usr/local/lib/R/site-library/ in container
+#set default renv cache path in container
+#change CRAN mirror from https://packagemanager.rstudio.com to AAU mirror
+#install renv, and install all packages in the lock file to /usr/local/lib/R/site-library/ in container
 RUN echo "RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER}" >> /usr/local/lib/R/etc/Renviron.site && \\
-  R -e "install.packages('renv', repos='http://cran.rstudio.com/', Ncpus = ${num_threads})" && \\
+  echo "options(repos = c(CRAN = 'https://mirrors.dotsrc.org/cran/'), download.file.method = 'libcurl')" >> /usr/local/lib/R/etc/Rprofile.site && \\
+  R -e "install.packages('renv', Ncpus = ${num_threads})" && \\
   R -e "renv::consent(provided = TRUE)" && \\
   R -e "renv::restore( \\
     library = '/usr/local/lib/R/site-library/', \\
