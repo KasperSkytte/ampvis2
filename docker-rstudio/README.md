94d392537b7e435ab730511f921f4431f2b25417
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-08-26 12:08:48 +0200

    write Dockerfile to a tmp dir instead+update readme

--- docker-rstudio/README.md
+++ docker-rstudio/README.md
@@ -1,26 +1,14 @@
 # RStudio docker container with ampvis2 pre-installed
-Use the docker container to run RStudio with a specific version of R as well as ampvis2 and all required dependencies pre-installed. 
+Use the docker container to run RStudio with a specific version of R and ampvis2 with all required dependencies pre-installed. 
 
-Run the `buildandrun.sh` script to build from scratch and start RStudio. This can take a while, but you can also just pull a prebuilt image directly from docker hub and run like below instead:
+Download and run the `buildandrun.sh` script to build from scratch and start RStudio. This can take a while, but you can also just pull a prebuilt image directly from docker hub instead by supplying `"pull"`, fx:
 
 ```
-r_ver="4.1.0"
-ampvis2_rel="2.7.8"
-image_name="kasperskytte/rstudio_r${r_ver}_ampvis2:${ampvis2_rel}"
-password="supersafepassword"
-port="8787"
-
-#pull image from docker hub
-docker pull ${image_name}
-
-#launch container
-docker run -d --rm \
-  -e "PASSWORD=${password}" \
-  -v "$HOME":/home/rstudio \
-  -p "$port":8787 \
-  ${image_name}
+curl -fsSL https://raw.githubusercontent.com/MadsAlbertsen/ampvis2/master/docker-rstudio/buildandrun.sh | bash -s "pull"
 ```
 
-Then access RStudio from a browser at http://127.0.0.1:${port}. To just use the latest build, set `ampvis2_rel="latest"`.
+A few environment variables can be set before running the script to adjust things: `r_ver`, `ampvis2_rel`, `image_name`, `password`, and `port`. Otherwise default values will be used.
 
-The `buildandrun.sh` script will by default also mount a host [renv](https://rstudio.github.io/renv/index.html) library cache to avoid having to install additional R packages with every container launch. Set `renv::settings$use.cache(FALSE)` in R to disable the cache if problems should occur due to fx mismatching shared libraries, or if you just want pure isolation with a project specific R library.
+You can then access RStudio from a browser at http://127.0.0.1:${port}.
+
+The `buildandrun.sh` script will by default also mount a host [renv](https://rstudio.github.io/renv/index.html) library cache to avoid having to install additional R packages with every container launch. Set `renv::settings$use.cache(FALSE)` in R to disable the cache if problems should occur due to fx mismatching shared libraries between the host and container, or if you just want pure isolation with a project specific R library.
--- docker-rstudio/buildandrun.sh
+++ docker-rstudio/buildandrun.sh
@@ -2,59 +2,15 @@
 set -eu
 
 #set variables
-r_ver="4.1.0"
-ampvis2_rel="2.7.8"
-image_name="kasperskytte/rstudio_r${r_ver}_ampvis2:${ampvis2_rel}"
-password="supersafepassword"
-port="8787" #will generate a random one if unavailable
+r_ver="${r_ver:-"4.1.0"}"
+ampvis2_rel="${ampvis2_rel:-"2.7.8"}"
+image_name="${image_name:-"kasperskytte/rstudio_r${r_ver}_ampvis2:${ampvis2_rel}"}"
+password="${password:-"supersafepassword"}"
+port="${port:-"8787"}" #will generate a random one if unavailable
 RENV_PATHS_CACHE_HOST="${HOME}/.local/share/renv/cache" #path to renv cache on host, renv default is ~/.local/share/renv/cache
 RENV_PATHS_CACHE_CONTAINER="/usr/local/lib/R/renv-cache/" #path to renv cache within the container (dont have to change)
 num_threads=$(($(nproc) - 2)) #all cores except 2
 #done setting variables
-
-cat << Dockerfile > Dockerfile
-FROM rocker/rstudio:${r_ver}
-
-#multithreaded make
-ENV MAKEFLAGS="-j ${num_threads} "
-ENV CRAN=https://mirrors.dotsrc.org/cran
-
-COPY renv.lock .
-
-#install nice-to-have system dependencies for R, and netstat to scan ports
-ARG DEBIAN_FRONTEND=noninteractive
-RUN apt-get update -qqy && \\
-  apt-get install -y --no-install-recommends --no-install-suggests \\
-    libxml2-dev \\
-    libcairo2-dev \\
-    libxt-dev \\
-    libjpeg-dev \\
-    net-tools \\
-    libharfbuzz-dev \\
-    libfribidi-dev \\
-    libfreetype6-dev \\
-    libpng-dev \\
-    libtiff5-dev \\
-    libjpeg-dev
-
-#set default renv cache path in container
-#change CRAN mirror from https://packagemanager.rstudio.com to AAU mirror
-#install renv, and install all packages in the lock file to /usr/local/lib/R/site-library/ in container
-RUN echo "RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER}" >> /usr/local/lib/R/etc/Renviron.site && \\
-  echo "options(repos = c(CRAN = 'https://mirrors.dotsrc.org/cran/'), download.file.method = 'libcurl')" >> /usr/local/lib/R/etc/Rprofile.site && \\
-  R -e "install.packages('renv', Ncpus = ${num_threads})" && \\
-  R -e "renv::consent(provided = TRUE)" && \\
-  R -e "renv::restore( \\
-    library = '/usr/local/lib/R/site-library/', \\
-    rebuild = TRUE, \\
-     clean = TRUE, \\
-     lockfile = 'renv.lock', \\
-     prompt = FALSE)"
-RUN R -e "renv::install('madsalbertsen/ampvis2@${ampvis2_rel}')"
-
-#silence RStudio warnings about not being able to write dictionary stuff to /root
-VOLUME /root
-Dockerfile
 
 #optionally pull the image instead of building
 pull=${1:-""}
@@ -63,8 +19,57 @@ then
   docker pull "${image_name}"
 else
   #build the container image
+  echo "Creating temporary folder for the Dockerfile..."
+  tmpdir=$(mktemp -dt "${image_name}_XXXXX")
+  pushd "$tmpdir" > /dev/null
+
+  cat << Dockerfile > Dockerfile
+  FROM rocker/rstudio:${r_ver}
+
+  #multithreaded make
+  ENV MAKEFLAGS="-j ${num_threads} "
+  ENV CRAN=https://mirrors.dotsrc.org/cran
+
+  COPY renv.lock .
+
+  #install nice-to-have system dependencies for R, and netstat to scan ports
+  ARG DEBIAN_FRONTEND=noninteractive
+  RUN apt-get update -qqy && \\
+    apt-get install -y --no-install-recommends --no-install-suggests \\
+      libxml2-dev \\
+      libcairo2-dev \\
+      libxt-dev \\
+      libjpeg-dev \\
+      net-tools \\
+      libharfbuzz-dev \\
+      libfribidi-dev \\
+      libfreetype6-dev \\
+      libpng-dev \\
+      libtiff5-dev \\
+      libjpeg-dev
+
+  #set default renv cache path in container
+  #change CRAN mirror from https://packagemanager.rstudio.com to AAU mirror
+  #install renv, and install all packages in the lock file to /usr/local/lib/R/site-library/ in container
+  RUN echo "RENV_PATHS_CACHE=${RENV_PATHS_CACHE_CONTAINER}" >> /usr/local/lib/R/etc/Renviron.site && \\
+    echo "options(repos = c(CRAN = 'https://mirrors.dotsrc.org/cran/'), download.file.method = 'libcurl')" >> /usr/local/lib/R/etc/Rprofile.site && \\
+    R -e "install.packages('renv', Ncpus = ${num_threads})" && \\
+    R -e "renv::consent(provided = TRUE)" && \\
+    R -e "renv::restore( \\
+      library = '/usr/local/lib/R/site-library/', \\
+      rebuild = TRUE, \\
+      clean = TRUE, \\
+      lockfile = 'renv.lock', \\
+      prompt = FALSE)"
+  RUN R -e "renv::install('madsalbertsen/ampvis2@${ampvis2_rel}')"
+
+  #silence RStudio warnings about not being able to write dictionary stuff to /root
+  VOLUME /root
+Dockerfile
   wget https://raw.githubusercontent.com/MadsAlbertsen/ampvis2/${ampvis2_rel}/renv.lock -O renv.lock
   docker build -t "${image_name}" .
+  echo "Removing temporary folder..."
+  popd > /dev/null && rm -rf "$tmpdir"
 fi
 
 checkPort() {
